850be099694fe2695612da58de42cef5b73460ab
racquemis <racquemis@users.noreply.github.com>
2021-10-07 12:12:23 +0200

    Add files via upload

--- cmd/main.go
+++ cmd/main.go
@@ -8,10 +8,10 @@ import (
 
 	// TODO: import your plugins
 	"github.com/influxdata/telegraf/plugins/common/shim"
-	_ "github.com/nicolasme/s7comm/plugins/inputs/s7comm"
+	_ "s7comm-main/plugins/inputs/s7comm"
 )
 
-var pollInterval = flag.Duration("poll_interval", 1*time.Second, "how often to send metrics")
+var pollInterval = time.Duration(10*time.Millisecond)
 var pollIntervalDisabled = flag.Bool("poll_interval_disabled", false, "set to true to disable polling. You want to use this when you are sending metrics on your own schedule")
 var configFile = flag.String("config", "", "path to the config file for this plugin")
 var err error
@@ -35,7 +35,7 @@ func main() {
 	// parse command line options
 	flag.Parse()
 	if *pollIntervalDisabled {
-		*pollInterval = shim.PollIntervalDisabled
+		pollInterval = shim.PollIntervalDisabled
 	}
 
 	// create the shim. This is what will run your plugins.
@@ -52,7 +52,7 @@ func main() {
 	}
 
 	// run a single plugin until stdin closes or we receive a termination signal
-	if err := shim.Run(*pollInterval); err != nil {
+	if err := shim.Run(pollInterval); err != nil {
 		fmt.Fprintf(os.Stderr, "Err: %s\n", err)
 		os.Exit(1)
 	}
--- plugins/inputs/s7comm/s7comm.go
+++ plugins/inputs/s7comm/s7comm.go
@@ -2,12 +2,35 @@ package s7comm
 
 import (
 	"time"
-
+    "strconv"
+	"strings"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/config"
 	"github.com/influxdata/telegraf/plugins/inputs"
 	"github.com/robinson/gos7"
 )
+
+const (
+	//S7 Datatypes
+	s7bool   = 0
+	s7byte	 = 1
+	s7word	 = 2
+	s7dword  = 3
+	s7int 	 = 4
+	s7dint   = 5
+	s7uint   = 6
+	s7udint  = 7
+	s7real   = 8
+	
+	//Areas
+	s7areape = 0x81 //Input
+	s7areapa = 0x82 //Output
+	s7areamk = 0x83 //Merker
+	s7areadb = 0x84 //Datablok
+	
+	MaxItemsPerReq = 18
+)
+
 
 // S7Comm
 type S7Comm struct {
@@ -18,7 +41,12 @@ type S7Comm struct {
 
 	Timeout     config.Duration `toml:"connect_timeout"`
 	IdleTimeout config.Duration `toml:"request_timeout"`
-
+	
+	PollInterval config.Duration `toml:"poll_interval"`
+	PollingDisabled bool `toml:"poll_interval_disabled"`
+    TicksPerInterval int64
+	TickCount int64
+	
 	Nodes []NodeSettings  `toml:"nodes"`
 	Log   telegraf.Logger `toml:"-"`
 
@@ -29,15 +57,21 @@ type S7Comm struct {
 }
 
 type NodeSettings struct {
+	MetricName	string `toml:"metric"`
 	Name    string `toml:"name"`
 	Address string `toml:"address"`
 	Type    string `toml:"type"`
+}
+type Metrics struct {
+	Name string
+	Count int
+	Nodes []NodeSettings
 }
 
 func (s *S7Comm) Connect() error {
 	s.handler = gos7.NewTCPClientHandler(s.Endpoint, s.Rack, s.Slot)
 	s.handler.Timeout = time.Duration(s.Timeout)
-	s.handler.IdleTimeout = time.Duration(s.IdleTimeout)
+	s.handler.IdleTimeout = time.Duration(s.IdleTimeout) //Must be equal or higher than PollingInterval set in main.go
 
 	err := s.handler.Connect()
 	if err != nil {
@@ -48,18 +82,24 @@ func (s *S7Comm) Connect() error {
 
 	s.client = gos7.NewClient(s.handler)
 
-	s.Log.Info("Connexion successfull")
+	//s.Log.Info("Connection successfull")
 
 	return nil
 }
 
 func (s *S7Comm) Stop() error {
 	err := s.handler.Close()
-
 	return err
 }
 
 func (s *S7Comm) Init() error {
+	if s.PollingDisabled {
+		s.TicksPerInterval = 1
+	
+	} else{
+		s.TicksPerInterval = int64(s.PollInterval) / int64(10 * time.Millisecond)
+	}
+	
 	err := s.Connect()
 	return err
 }
@@ -86,69 +126,205 @@ func (s *S7Comm) SampleConfig() string {
 }
 
 func (s *S7Comm) Gather(a telegraf.Accumulator) error {
-
+	if s.TickCount == s.TicksPerInterval{
+		s.TickCount = 0
+	} else{
+		s.TickCount++
+		return nil
+	}
+	
+	var metrics []Metrics
+	//Determine number of unique metrics
 	for _, node := range s.Nodes {
-
-		var fields map[string]interface{}
-		buf := make([]byte, 4)
-
-		_, err := s.client.Read(node.Address, buf)
-
-		if err != nil {
-			s.Log.Error(err)
-		} else {
-			switch dataType := node.Type; dataType {
-			case "bool":
-				var res bool
-				s.helper.GetValueAt(buf, 0, &res)
-				fields = map[string]interface{}{node.Name: res}
-			case "byte":
-				var res byte
-				s.helper.GetValueAt(buf, 0, &res)
-				fields = map[string]interface{}{node.Name: res}
-			case "word":
-				var res uint16
-				s.helper.GetValueAt(buf, 0, &res)
-				fields = map[string]interface{}{node.Name: res}
-			case "dword":
-				var res uint32
-				s.helper.GetValueAt(buf, 0, &res)
-				fields = map[string]interface{}{node.Name: res}
-			case "int":
-				var res int16
-				s.helper.GetValueAt(buf, 0, &res)
-				fields = map[string]interface{}{node.Name: res}
-			case "dint":
-				var res int32
-				s.helper.GetValueAt(buf, 0, &res)
-				fields = map[string]interface{}{node.Name: res}
-			case "uint":
-				var res uint16
-				s.helper.GetValueAt(buf, 0, &res)
-				fields = map[string]interface{}{node.Name: res}
-			case "udint":
-				var res uint32
-				s.helper.GetValueAt(buf, 0, &res)
-				fields = map[string]interface{}{node.Name: res}
-			case "real":
-				var res float32
-				s.helper.GetValueAt(buf, 0, &res)
-				fields = map[string]interface{}{node.Name: res}
-			case "time":
-				var res uint32
-				s.helper.GetValueAt(buf, 0, &res)
-				fields = map[string]interface{}{node.Name: res}
-			default:
-				s.Log.Error("Type error - ", node.Type)
-				fields = nil
-			}
-
-			if fields != nil {
-				a.AddFields(s.MetricName, fields, nil)
+		found := false
+		out:
+		for j,  Metric := range metrics {
+			if node.MetricName == Metric.Name {
+				found = true
+				metrics[j].Count++
+				metrics[j].Nodes = append(metrics[j].Nodes,node)
+				
+				break out
 			}
 		}
+		if found == false {
+			var Node []NodeSettings
+			Node= append(Node,node)
+			metrics = append(metrics,Metrics{Name: node.MetricName,Count: 1, Nodes: Node})
+		}
 	}
+	
+	for _,  metric := range metrics {
+		var pos, offset, s7_size, s7_dbNo,s7_dbIndex, s7_area, s7_bit int
+		buf2 := make([]byte, 72)
+		var items []gos7.S7DataItem 
+		var dtype []int
+		var errmsg [18]string
+		var fields = make(map[string]interface{})
+		for _, node := range metric.Nodes {
+			if node.MetricName == metric.Name {
+				switch Area := node.Address[0:2]; Area {
+					case "EB","IB","AB","QB","MB","EW","IW","AW","QW","MW","ED","ID","AD","QD","MD": //input byte
+						ioIndex, _ := strconv.ParseInt(string(string(node.Address)[2:]), 10, 16)
+						switch ioType := node.Address[0:1]; ioType {
+							case "E","I":
+								s7_area = s7areape
+							case "A","Q":
+								s7_area = s7areapa
+						}
+						switch ioSize := Area[1:2]; ioSize {
+							case "B":
+								s7_size = 1
+							case "W":
+								s7_size = 2
+							case "D":
+								s7_size = 4
+						}
+						s7_dbIndex, s7_dbNo = int(ioIndex), 0
+					case "DB": //Data Block
+						dbArray := strings.Split(node.Address, ".")
+						if len(dbArray) < 2 {
+							s.Log.Error("Db Area read variable should not be empty")
+							continue
+						}
+						dbNo, _:= strconv.ParseInt(string(string(dbArray[0])[2:]), 10, 16)
+						dbIndex, _ := strconv.ParseInt(string(string(dbArray[1])[3:]), 10, 16)
+						dbType := string(dbArray[1])[0:3]
 
+						s7_dbIndex, s7_dbNo, s7_area = int(dbIndex), int(dbNo), s7areadb
+						switch dbType {
+							case "DBB": //byte
+								s7_size = 1
+							case "DBX": //bit
+								s7_size = 1
+								dbBit, _ := strconv.ParseInt(string(string(dbArray[2])[0:]),10,16)
+								if dbBit > 7 || dbBit < 0{
+									s.Log.Error("Db Read Bit is invalid for address ",node.Address)
+									continue
+								}
+								s7_bit = int(dbBit) << 4
+							case "DBW": //word
+								s7_size = 2
+							case "DBD": //dword
+								s7_size = 4
+							default:
+								s.Log.Error("Error when parsing dbtype for address ",node.Address)
+								continue
+						}
+						
+					default:
+						switch otherArea := node.Address[0:1]; otherArea {
+							case "I","E": //input
+								s7_area = s7areape
+							case "0","A": //output
+								s7_area = s7areapa
+							case "M": //memory
+								s7_area = s7areamk 
+							default:
+								s.Log.Error("Error when parsing dbtype for address ",node.Address)
+								continue
+						}
+						ioArray := strings.Split(node.Address, ".")
+						ioIndex, _ := strconv.ParseInt(string(string(ioArray[0])[1:]), 10, 16)
+						ioBit, _ := strconv.ParseInt(string(string(ioArray[1])[0:]),10,16)
+						if ioBit > 7 || ioBit < 0{
+							s.Log.Error("Db Read Bit is invalid for address ",node.Address)
+							continue
+						}
+						s7_dbIndex, s7_dbNo, s7_size, s7_bit = int(ioIndex), 0, 1, int(ioBit) << 4
+				}
+				switch dataType := node.Type; dataType {
+					case "bool":
+						dtype = append(dtype,s7_bit)
+					case "byte":
+						dtype = append(dtype,s7byte)
+					case "word":
+						dtype = append(dtype,s7word)
+					case "dword":
+						dtype = append(dtype,s7dword)
+					case "int":
+						dtype = append(dtype,s7int)
+					case "dint", "time":
+						dtype = append(dtype,s7dint)
+					case "uint":
+						dtype = append(dtype,s7uint)
+					case "udint":
+						dtype = append(dtype,s7udint)
+					case "real":
+						dtype = append(dtype,s7real)
+					default:
+						s.Log.Error("Unknown datatype '", dataType, "' for ", node.Address)
+						continue
+				}
+				items = append(items,gos7.S7DataItem{
+					Area:     s7_area,
+					WordLen:  0x02,
+					DBNumber: s7_dbNo,
+					Start:    s7_dbIndex,
+					Amount:   s7_size,
+					Data:     buf2[pos*4:],
+					Error:    errmsg[pos]})
+				pos++
+			}
+			if len(items) == MaxItemsPerReq || len(items)+offset == metric.Count{
+				err := s.client.AGReadMulti(items, pos)
+				if err != nil {
+					s.Log.Error(err)
+				} else {
+					for i := 0; i < len(items); i++ {
+						switch dataType := dtype[i] & 0x0F; dataType {
+						case s7bool:
+							var res byte
+							mask:= []byte{0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80}
+							bit:= dtype[i] >> 4
+							s.helper.GetValueAt(buf2[i*4:], 0, &res)
+							var val bool = (res & mask[bit]) != 0
+							fields[metric.Nodes[offset+i].Name] = val
+						case s7byte:
+							var res byte
+							s.helper.GetValueAt(buf2[i*4:], 0, &res)
+							fields[metric.Nodes[offset+i].Name] = res
+						case s7word:
+							var res uint16
+							s.helper.GetValueAt(buf2[i*4:], 0, &res)
+							fields[metric.Nodes[offset+i].Name] = res
+						case s7dword:
+							var res uint32
+							s.helper.GetValueAt(buf2[i*4:], 0, &res)
+							fields[metric.Nodes[offset+i].Name] = res
+						case s7int:
+							var res int16
+							s.helper.GetValueAt(buf2[i*4:], 0, &res)
+							fields[metric.Nodes[offset+i].Name] = res
+						case s7dint:
+							var res int32
+							s.helper.GetValueAt(buf2[i*4:], 0, &res)
+							fields[metric.Nodes[offset+i].Name] = res
+						case s7uint:
+							var res uint16
+							s.helper.GetValueAt(buf2[i*4:], 0, &res)
+							fields[metric.Nodes[offset+i].Name] = res
+						case s7udint:
+							var res uint32
+							s.helper.GetValueAt(buf2[i*4:], 0, &res)
+							fields[metric.Nodes[offset+i].Name] = res
+						case s7real:
+							var res float32
+							s.helper.GetValueAt(buf2[i*4:], 0, &res)
+							fields[metric.Nodes[offset+i].Name] = res
+						}
+					}
+				}
+				offset, dtype, items = pos, nil, nil
+				pos = 0
+			}
+		}
+		if fields != nil {
+			a.AddFields(metric.Name, fields, nil)
+			dtype, items = nil, nil
+			pos = 0
+		}
+	}
 	return nil
 }
 
